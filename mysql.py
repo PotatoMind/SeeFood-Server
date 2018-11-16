264302f721c3a308a1901e7663f34677bda0a825
Trevor Konya <trevor.konya@gmail.com>
2018-11-16 12:49:07 -0500

    Update to create query in mysql.py
    Added columns for future use, wrapped column names and table name in backticks

--- mysql.py
+++ mysql.py
@@ -6,8 +6,11 @@ sql = "CREATE DATABASE IF NOT EXISTS SeeFood;"
 cursor.execute(sql)
 sql = "USE SeeFood;"
 cursor.execute(sql)
-sql = '''CREATE TABLE IF NOT EXISTS IMAGES ( 
-    NAME VARCHAR(255) NOT NULL, 
-    PATH VARCHAR(255) NOT NULL );'''
+sql = '''CREATE TABLE IF NOT EXISTS `IMAGES` ( 
+    `NAME` VARCHAR(191) NOT NULL COLLATE utf8mb4_bin, 
+    `PATH` VARCHAR(255) NOT NULL COLLATE utf8mb4_bin,
+    `SCORE_1` FLOAT NULL,
+    `SCORE_2` FLOAT NULL,
+    `HASH` CHAR(32) NULL);'''
 cursor.execute(sql)
 db.close()
